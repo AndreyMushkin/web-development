555a23af32a5b9f64825bcb4e0c7b173e7f7d8d7
AndreyMushkin <222mushkin@gmail.com>
2021-02-27 18:45:49 +0300

    lw3 task1

---
+++ lw3/task1.php
@@ -0,0 +1,26 @@
+<?php
+$string = $_GET['text'];
+$index = 0;
+$resultString = '';
+
+header("Content-Type: text/plain");
+
+while ($string[$index] === ' ')
+{
+    $index += 1;
+}
+
+while ($string[$index] !== ' ')
+{
+    $resultString .= $string[$index];
+    $index += 1;
+    if ($string[$index] === ' ')
+    {
+        $resultString .= ' ';
+        while ($string[$index] === ' ')
+	{
+	    $index += 1;
+	}
+    }
+}
+echo $resultString;		
