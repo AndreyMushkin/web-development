08546de6c20dacd1b2b1e3ce7ca20fb0c99c7144
AndreyMushkin <222mushkin@gmail.com>
2021-03-04 21:55:16 +0300

    removed comments

--- lw3/task2.php
+++ lw3/task2.php
@@ -6,14 +6,12 @@ $index = 0;
 
 header('Content-Type: text/plain');
 
-//проверка на пустую строку
 if (strlen($identifier) == 0)
 {
     $isIdentifier = false;
     echo 'No, empty string.';
 }
 
-//проверка на цифру в первой позиции
 if (is_numeric($identifier[$index]))
 {
     $isIdentifier = false;
@@ -21,10 +19,9 @@ if (is_numeric($identifier[$index]))
     ++$index;    
 }
 
-//проверка на недопустимые символы
 while (($index <= strlen($identifier)) and $isIdentifier)
 {
-    //если символ не(буква или цифра) 
+    //if not(letter or digit) 
     if (!(ctype_alpha($identifier[$index].'a') or is_numeric($identifier[$index])))
     {
         $isIdentifier = false;
