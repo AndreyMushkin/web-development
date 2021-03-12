2622b7a427bb805dcd1dcfab42ef48ad1d95a569
AndreyMushkin <222mushkin@gmail.com>
2021-03-12 10:42:13 +0300

    replaced tabs with spaces

--- lw3/task3.php
+++ lw3/task3.php
@@ -15,55 +15,55 @@ $lowerLetterNumber = 0;
 $i = 0;
 while ($i < $passwordLength)
 {
-	if (is_numeric($password[$i]))
-	{
-		++$digitNumber;
-		$passwordStrength += 4;
-	}
-	else
-	{
-		if (ctype_upper($password[$i]))
-		{
-			++$upperLetterNumber;
-		}
-		else
-		{
-			++$lowerLetterNumber;
-		}
-	}
-	++$i;
+    if (is_numeric($password[$i]))
+    {
+        ++$digitNumber;
+        $passwordStrength += 4;
+    }
+    else
+    {
+        if (ctype_upper($password[$i]))
+        {
+            ++$upperLetterNumber;
+        }
+        else
+        {
+            ++$lowerLetterNumber;
+        }
+    }
+    ++$i;
 }
 
 if ($upperLetterNumber > 0)
 {
-	$passwordStrength += ($passwordLength - $upperLetterNumber) * 2;
+    $passwordStrength += ($passwordLength - $upperLetterNumber) * 2;
 }
 
 if ($lowerLetterNumber > 0)
 {
-	$passwordStrength += ($passwordLength - $lowerLetterNumber) * 2;
+    $passwordStrength += ($passwordLength - $lowerLetterNumber) * 2;
 }
 
 //if only digits
 if ($digitNumber === $passwordLength)
 {
-	$passwordStrength -= $passwordLength;
+    $passwordStrength -= $passwordLength;
 }
 
 //if only letters
 if ($digitNumber === 0)
 {
-	$passwordStrength -= $passwordLength;
+    $passwordStrength -= $passwordLength;
 }
 
 $countedPassword = array_count_values(str_split($password));  //array with counts of each character
 
 for($i = 0; $i <= count($countedPassword); ++$i)
 {
-	if ($countedPassword[$i] > 1)
-	{
-		$passwordStrength -= $countedPassword[$i];
-	}
+    if ($countedPassword[$i] > 1)
+    {
+        $passwordStrength -= $countedPassword[$i];
+    }
 }
 
 echo 'Password strength is '. $passwordStrength;
