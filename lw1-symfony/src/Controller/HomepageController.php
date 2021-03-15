46f55c7e91b1eaaebd8ec86f6ae66e45e0d2f0d5
AndreyMushkin <222mushkin@gmail.com>
2021-03-15 18:51:40 +0300

    fixed mistakes

--- lw1-symfony/src/Controller/HomepageController.php
+++ lw1-symfony/src/Controller/HomepageController.php
@@ -6,20 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomepageController extends AbstractController
-{      
-
+{
     public function homepage(): Response
     {
-        
-	$name = 'Andrey Mushkin';
-	$age = '19';
-	$email = '222mushkin@gmail.com';
+    $name = 'Andrey Mushkin';
+    $age = '19';
+    $email = '222mushkin@gmail.com';
 
-    return $this->render('homepage/homepage.html.twig', [
-        'name' => $name,
-        'age' => $age,
-        'email' => $email,
+        return $this->render('homepage/homepage.html.twig', [
+            'name' => $name,
+            'age' => $age,
+            'email' => $email,
         ]);
     }
-
 }
